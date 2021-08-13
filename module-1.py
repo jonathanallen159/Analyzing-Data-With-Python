beccdd4b7089b9be624e3d6314c99b2ada6ad4f4
jonathanallen159 <j.allen96@virginmedia.com>
2021-08-13 12:49:15 +0100

    First atom commit

---
+++ module-1.py
@@ -0,0 +1,9 @@
+# MODULE 1
+# this programme is the accompanying coursework to module 1 of the analyzing data with python IBM coursework
+
+''' LEARNING OBJECTIVES:
+ - analyze python data using a dataset
+ - identify three python libraries and describe their uses
+ - read data using python's Pandas package
+ - demonstrate how to import data and export in python
+'''
